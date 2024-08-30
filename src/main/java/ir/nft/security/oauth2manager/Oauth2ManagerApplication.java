ad20d28852029b1629a6f5b1867e97f8ff14e46f
Erfan <erfanscott2@gmail.com>
2024-08-30 10:28:56 +0330

    chore: create initial project setup

---
+++ src/main/java/ir/nft/security/oauth2manager/Oauth2ManagerApplication.java
@@ -0,0 +1,13 @@
+package ir.nft.security.oauth2manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ManagerApplication.class, args);
+	}
+
+}
