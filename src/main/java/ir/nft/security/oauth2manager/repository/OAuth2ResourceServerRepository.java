19458ea105b4336de2aea927181019ff134ae6bf
Erfan <erfanscott2@gmail.com>
2024-09-10 18:54:38 +0330

    refactor(Repository): apply the single table strategy to registered clients

--- src/main/java/ir/nft/security/oauth2manager/repository/OAuth2ResourceServerRepository.java
+++
@@ -1,6 +0,0 @@
-package ir.nft.security.oauth2manager.repository;
-
-import ir.nft.security.oauth2manager.entity.OAuth2ResourceServer;
-
-public interface OAuth2ResourceServerRepository
-    extends OAuth2RegisteredClientRepository<OAuth2ResourceServer> {}
